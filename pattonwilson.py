5c0568acd38336b4c6e6a2de0c581dbf82e0ebe6
tahkmahnelle <137662409+DJamesVersion@users.noreply.github.com>
2023-10-26 14:50:14 -0700

    Add files via upload

---
+++ pattonwilson.py
@@ -0,0 +1,21 @@
+print ("hello, this is PattonWilson; I am pleased to make you needs available for this program...please preview with focus and attention-these files are for public consumption and are very personable_in other words: have fun, but be carefull. ENJOY!")
+print ("As well, Please feel free to do whatever you will with these files...")
+print ("There some rules; next you must answer some questions, in order for yourselves to become familiar with the EHDPROGS and the files alongside-->so that you will have the availability to recieve the most out of these files, also. At least we hope, so to say.");
+
+input ("Please tell us that you like music, esp. techno or folk-y or n:")
+print ("If you said yes then please write on a peice of paper the word ECHO, if you said no then write the word HOTEL, if you wrote other than y or no, then write the word DELTA.")
+
+input ("If you like science, agriculture, or the arts, or if you like engineering and/or medicine then please note-a, b, c, or d, e for otherwise:")
+print ("Now, please if you have said a write on a peice of paper the wrd ROCKET, if you have said b, then write the word TAVERN, if you have said c, then write the word YELLOW, if you have said d, then write the word KNOWLEDGE.")
+
+input ("Next, I must ask you if you have ever heard of the mathematic equation x=abcd? y or n:")
+print ("If you said y, then write on a peice of paper the word PLUS, if you said n, then write MULTIPLY, if you said any other, then write PLUS.")
+
+input ("Finally, the last question_what is the final sum of e at x=abcd?:")
+print ("This question may be quite opinionated in response, yet if you said 24 or omega, then the answer is y.")
+
+input ("REMEMBER THE WORDS YOU WROTE DOWN-YOU WILL NEED THESE WORDS FOR LATER IN ORDER TO STAY FIT WITH DIRECTION AND MEANING FOR YOUR BENEFIT AND PURPOSE. Something of a HINT now to HELP you on your way...a positron or a nuetron or a ion or an electron? No matter what answer you give, even A=B*, or E=MC^2, or how did the dinosaur get eggs before the chicken? Or maybe that the Jesus might have turned coke into pepsi and did not tell us this? or that there is absolutely no value in paleobotonic narcotics and saratonin?Still noone may ever be certain. Yet x may and may not equal 24-though 1 * 2 * 3* 4=24 and x and omega are of the number 24 in phonology. As well we all LIVE in a world of baryonic matter, that in which is at this moment, only proven so far to be 4 dimensions-three space and a time. NOW PLEASE PROCEED and type the word IOTA:")
+print ("THANK YOU, processing you information to the highest degree of perfection. STAND BY. .. ... .... ..... ...... ....... ........ .........")
+
+
+
